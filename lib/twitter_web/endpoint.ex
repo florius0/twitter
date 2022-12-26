ba2bd2f52357418aa034966c48da04c5abb673f7
Vadim Tsvetkov <vadim.tsvetkov80@gmail.com>
2022-12-26 05:18:31 +0300

    chore: remove static files & assets

--- lib/twitter_web/endpoint.ex
+++ lib/twitter_web/endpoint.ex
@@ -10,18 +10,6 @@ defmodule TwitterWeb.Endpoint do
     signing_salt: "FXpcnEaT"
   ]
 
-  # socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
-
-  # Serve at "/" the static files from "priv/static" directory.
-  #
-  # You should set gzip to true if you are running phx.digest
-  # when deploying your static files in production.
-  plug Plug.Static,
-    at: "/",
-    from: :twitter,
-    gzip: false,
-    only: ~w(assets fonts images favicon.ico robots.txt)
-
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
   if code_reloading? do
